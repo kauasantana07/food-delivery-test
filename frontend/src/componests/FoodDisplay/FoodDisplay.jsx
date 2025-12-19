684ba05247cd19169ea49afb78e0fb47babdc001
kauacomk123 <kauasantana2707@gmail.com>
2025-12-18 21:11:32 -0300

    adcionei novas funcionalidade,consertei um buf no arquivo FoodDisplay.jsx que nao estava passando o id correto dos itens

--- frontend/src/componests/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/componests/FoodDisplay/FoodDisplay.jsx
@@ -11,8 +11,8 @@ const FoodDisplay = ({category}) => {
     <div className='food-display' id='food-display'>
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
-       {food_list.map((item,index)=>{
-         return <FoodItem key={index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image} />
+       {food_list.map((item,index)=>{//encontrei um bug aqui item.id mudei para item._id
+         return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
        })}
       </div>
     </div>
